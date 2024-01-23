5bce3158f2d59e3638155a3b3c223f10be658ceb
12Acorns <nathanrutter09@gmail.com>
2024-01-23 21:19:23 +0000

    Commit

---
+++ FindMissingElementApp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FindMissingElementApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] _values = new int[]
+			{
+				3, 0, 1
+			};
+			Console.WriteLine(FindMissingElement(_values));
+		}
+		/// <summary>
+		/// Expects an array to start at 0 and increment by one.
+		/// </summary>
+		static int FindMissingElement(int[] _array)
+		{
+			if(_array.Min() != 0)
+			{
+				throw new ArgumentException("Invalid input", nameof(_array));
+			}
+			Dictionary<int, int> _weightArray = new Dictionary<int, int>();
+			for(int i = 0; i < _array.Max(); i++)
+			{
+				_weightArray.Add(i, 0);
+			}
+			for(int i = 0; i < _array.Length; i++)
+			{
+				_weightArray[_array[i]] = 1;
+			}
+			return _weightArray.FirstOrDefault(x => x.Value == 0).Key;
+		}
+	}
+}
